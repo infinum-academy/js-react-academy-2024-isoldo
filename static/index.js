69687ff6e85cd94e5c552c1a9e9fcee5479297c5
Ivan Soldo <ivan@calimero.network>
2024-07-03 22:20:56 +0200

    js3: implement average rating

--- static/index.js
+++ static/index.js
@@ -14,15 +14,33 @@ let reviewList = [
 
 const render = () => {
   const ul = document.createElement('ul');
+  let sum = 0;
 
   reviewList.forEach((r) => {
     const reviewItem = createReview(r);
     ul.appendChild(reviewItem);
+    sum += r.rating;
   });
 
-  const el = document.getElementById('reviews');
-  el.innerHTML = '';
-  el.appendChild(ul);
+  const reviews = document.getElementById('reviews');
+  reviews.innerHTML = '';
+  reviews.appendChild(ul);
+
+  const avgRating = sum/reviewList.length;
+
+  const showAvgRatingElement = document.getElementById('show-avg-rating');
+  if(showAvgRatingElement) {
+    console.warn('Removing existing avg div');
+    showAvgRatingElement.parentNode.removeChild(showAvgRatingElement);
+  }
+
+  const newShowAvgRatingElementDiv = document.createElement('div');
+  newShowAvgRatingElementDiv.id = 'show-avg-rating';
+  const newShowAvgRatingElementSpan = document.createElement('span');
+  newShowAvgRatingElementSpan.textContent = avgRating.toString() + '/' + MAX_RATING.toString();
+  newShowAvgRatingElementDiv.appendChild(newShowAvgRatingElementSpan);
+
+  document.getElementById('show-content').appendChild(newShowAvgRatingElementDiv);
 }
 
 const createReview = (r) => {

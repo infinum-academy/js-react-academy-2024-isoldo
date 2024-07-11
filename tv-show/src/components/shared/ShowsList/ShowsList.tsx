1a1ef07d78b6ad8a49e75a7fbae63ab2be003edd
Ivan Soldo <ivan@calimero.network>
2024-07-12 00:58:54 +0200

    extract common logic from all-shows and top-rated to ShowsList

--- tv-show/src/components/shared/ShowsList/ShowsList.tsx
+++ tv-show/src/components/shared/ShowsList/ShowsList.tsx
@@ -1,17 +1,37 @@
 import { IShow } from "@/typings/Show.type";
-import { SimpleGrid } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Spinner } from "@chakra-ui/react";
 import ShowCard from "../ShowCard/ShowCard";
+import ErrorBox from "../ErrorBox/ErrorBox";
+import useSWR from "swr";
+import { IShowsResponse } from "@/fetchers/shows";
 
 interface IShowsListProps {
-  showsList: IShow[];
+  swrKey: string;
+  fetcher: () => Promise<IShowsResponse>;
 }
 
-export default function ShowsList({showsList}: IShowsListProps) {
+export default function ShowsList({swrKey, fetcher}: IShowsListProps) {
+  const { data, error, isLoading } = useSWR(swrKey, fetcher);
+
+  if(error) {
+    return (
+      <ErrorBox title='Error loading data' description={`${error}`}/>
+    );
+  }
+
+  // (!data) here to calm the linter down - data will be defined is both error and isLoading are nullish
+  if(isLoading || !data) {
+    return (
+      <Flex justifyContent='center'>
+        <Spinner size='xl'/>
+      </Flex>
+    );
+  }
   return (
       <SimpleGrid minChildWidth='240px'>
       {
-        showsList.map((show, index) => { return (
-            <ShowCard key={index} title={show.title} imageUrl={show.image_url} averageRating={show.average_rating} />
+        (data.shows as IShow[]).map((show) => { return (
+            <ShowCard key={show.id} title={show.title} imageUrl={show.image_url} averageRating={show.average_rating} />
         )})
       }
       </SimpleGrid>

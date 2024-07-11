a976e79dd30ec707c3b4366bd91a5bcee15a0234
Ivan Soldo <ivan@calimero.network>
2024-07-12 00:58:54 +0200

    extract common logic from all-shows and top-rated to ShowsList

--- tv-show/src/app/top-rated/page.tsx
+++ tv-show/src/app/top-rated/page.tsx
@@ -1,28 +1,8 @@
 'use client';
 
-import ErrorBox from "@/components/shared/ErrorBox/ErrorBox";
-import ShowsList from "@/components/shared/ShowsList/ShowsList"
+import ShowsList from "@/components/shared/ShowsList/ShowsList";
 import { getTopRatedShowsDetails } from "@/fetchers/shows";
-import { Flex, Spinner } from "@chakra-ui/react";
-import useSWR from "swr";
 
 export default function ShowsListTopPage() {
-  const { data, error, isLoading } = useSWR(`/top-rated`, () => getTopRatedShowsDetails());
-
-  if(error) {
-    return (
-      <ErrorBox title='Error loading data' description={`${error}`}/>
-    );
-  }
-
-  // (!data) here to calm the linter down - data will be defined is both error and isLoading are nullish
-  if(isLoading || !data) {
-    return (
-      <Flex justifyContent='center'>
-        <Spinner size='xl'/>
-      </Flex>
-    );
-  }
-
-  return <ShowsList showsList={data.shows} />;
+  return <ShowsList swrKey='/top-rated' fetcher={() => getTopRatedShowsDetails()} />;
 }

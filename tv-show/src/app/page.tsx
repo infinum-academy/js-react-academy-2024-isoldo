5c2d5cf0b471cc5d75f3624d8c2493f0cc6442cc
Ivan Soldo <ivan@calimero.network>
2024-07-12 11:51:15 +0200

    redirect root to all-shows

--- tv-show/src/app/page.tsx
+++ tv-show/src/app/page.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import { Container, Heading } from "@chakra-ui/react";
 import styles from "./page.module.css";
-import ShowContainer from "@/components/features/shows/ShowContainer/ShowContainer";
-
-const APP_NAME = "TV Shows App"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  router.push('/all-shows');
+
   return (
     <main className={styles.main}>
-      <Container>
-        <Heading>{APP_NAME}</Heading>
-        <ShowContainer />
-      </Container>
     </main>
   );
 }

b7e322cc86363e8d8b671d4415f447c58510eec5
Ivan Soldo <ivan@calimero.network>
2024-07-12 11:51:02 +0200

    implement navigation on click on sidenavbar

--- tv-show/src/components/shared/SidebarNavigation/SidebarNavigation.tsx
+++ tv-show/src/components/shared/SidebarNavigation/SidebarNavigation.tsx
@@ -1,11 +1,12 @@
 import { Button, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function SidebarNavigation() {
   return (
     <VStack h='100vh' justifyContent='space-between'>
       <VStack>
-        <Button>All shows</Button>
-        <Button>Top rated</Button>
+        <Button href='/all-shows' as={Link}>All shows</Button>
+        <Button href='/top-rated' as={Link}>Top rated</Button>
         <Button>My Profile</Button>
       </VStack>
       <Button>Log out</Button>

74724226a2d0bbcede3c2cf353587b0feeae1964
Ivan Soldo <ivan@calimero.network>
2024-07-14 11:47:21 +0200

    add highlight to sidebar navigation

--- tv-show/src/components/shared/SidebarNavigation/SidebarNavigation.tsx
+++ tv-show/src/components/shared/SidebarNavigation/SidebarNavigation.tsx
@@ -1,15 +1,36 @@
+'use client';
+
 import { Button, VStack } from "@chakra-ui/react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function SidebarNavigation() {
+  const colorScheme = "orange";
   return (
     <VStack h='100vh' justifyContent='space-between' position='sticky' top='0px'>
       <VStack>
-        <Button href='/all-shows' as={Link}>All shows</Button>
-        <Button href='/top-rated' as={Link}>Top rated</Button>
-        <Button>My Profile</Button>
+        <ButtonLinkWithSelectHighlight text='All shows' href='/all-shows' colorScheme={colorScheme} />
+        <ButtonLinkWithSelectHighlight text='Top rated' href='/top-rated' colorScheme={colorScheme} />
+        <ButtonLinkWithSelectHighlight text='My Profile' colorScheme={colorScheme} />
       </VStack>
-      <Button>Log out</Button>
+      <ButtonLinkWithSelectHighlight text='Log out' colorScheme={colorScheme} />
+
     </VStack>
   );
+}
+
+interface IButtonLinkWithSelectHighlight {
+  text: string;
+  href?: string;
+  colorScheme: string;
+}
+function ButtonLinkWithSelectHighlight({text, href, colorScheme}: IButtonLinkWithSelectHighlight) {
+  const path = usePathname();
+  const isSelectedVariant = () => {return href === path ? 'solid' : 'outline'};
+
+  if(href) {
+    return <Button as={Link} href={href} colorScheme={colorScheme} variant={isSelectedVariant()}>{text}</Button>
+  }
+  return <Button colorScheme={colorScheme} variant={isSelectedVariant()}>{text}</Button>
+
 }

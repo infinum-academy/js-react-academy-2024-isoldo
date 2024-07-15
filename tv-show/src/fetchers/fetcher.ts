e4cf004e8ba6588e572b87c92d2215f160074043
Ivan Soldo <ivan@calimero.network>
2024-07-15 21:12:18 +0200

    reuse fetcher for authenticatedFetcher

--- tv-show/src/fetchers/fetcher.ts
+++ tv-show/src/fetchers/fetcher.ts
@@ -20,22 +20,5 @@ export async function fetcher<T>(input: string | URL | Request, init?: RequestIn
 
 export async function authenticatedFetcher<T>(input: string | URL | Request, init?: RequestInit | undefined): Promise<T> {
   const headers = JSON.parse(localStorage.getItem("register-response-headers") || "{}");
-  try {
-    const response = await fetch(input, {
-      ...init,
-      headers: {
-        ...init?.headers,
-        ...headers
-      }
-    });
-    if(!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-
-    return json;
-  } catch(error) {
-    throw new Error(`Response: ${error}`);
-  }
+  return fetcher<T>(input, {...init, headers: {...headers}});
 }

2c27b5ebacfce37db527a54b8ecf366f53080230
Ivan Soldo <ivan@calimero.network>
2024-07-15 11:50:41 +0200

    add swrKeys getter object

---
+++ tv-show/src/fetchers/swrKeys.ts
@@ -0,0 +1,10 @@
+const API_URL = 'https://tv-shows.infinum.academy';
+
+export const swrKeys = {
+  user: () => `${API_URL}/users/me`,
+  login: () => `${API_URL}/users/sign_in`,
+  register: () => `${API_URL}/users/`,
+  all_shows: () => `${API_URL}/shows`,
+  top_rated: () => `${API_URL}/shows/top_rated`,
+  show: (id: number) => `${API_URL}/shows/${id}`
+};

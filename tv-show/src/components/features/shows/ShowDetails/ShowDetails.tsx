0eeee327508ea390f7a5c0639ec1fba97042bc2b
Ivan Soldo <ivan@calimero.network>
2024-07-27 22:43:36 +0200

    update ShowDetails layout

--- tv-show/src/components/features/shows/ShowDetails/ShowDetails.tsx
+++ tv-show/src/components/features/shows/ShowDetails/ShowDetails.tsx
@@ -1,5 +1,6 @@
 import { IShow } from "@/typings/Show.type";
-import { Box, Card, CardBody, CardHeader, Container, Heading, Image, Stack, StackDivider } from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
+import { Box, Card, CardBody, CardHeader, Container, Flex, Heading, Image, Stack, StackDivider } from "@chakra-ui/react";
 
 function getAverageRatingText(averageRating: number | undefined) {
   return (typeof averageRating !== 'undefined') ?
@@ -19,7 +20,7 @@ export default function ShowDetails({show, averageRating}: IShowDetailsProps) {
   return (
     <Container>
       <Card marginBottom={8}>
-        <CardHeader>
+        <CardHeader padding="0">
           <Image src={image_url} fallbackSrc='https://fakeimg.pl/600x400/353b38/e85115?text=TV+Show' pb={4}/>
           <Heading size='md'>
             {title}
@@ -32,7 +33,10 @@ export default function ShowDetails({show, averageRating}: IShowDetailsProps) {
               {description}
             </Box>
             <Box>
-              {averageRatingText}
+              <Flex alignItems="center" gap={2}>
+                <StarIcon />
+                {averageRatingText}
+              </Flex>
             </Box>
           </Stack>
         </CardBody>

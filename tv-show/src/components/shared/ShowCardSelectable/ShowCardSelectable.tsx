9206175ab254b22ac8ada0988efee18f143b8d1f
Ivan Soldo <ivan@calimero.network>
2024-07-29 00:57:24 +0200

    remove avg rating from picker, hide images on small screens

--- tv-show/src/components/shared/ShowCardSelectable/ShowCardSelectable.tsx
+++ tv-show/src/components/shared/ShowCardSelectable/ShowCardSelectable.tsx
@@ -1,5 +1,5 @@
 import { IShow } from "@/typings/Show.type";
-import { Button, Card, CardBody, Flex, Image, Text } from "@chakra-ui/react";
+import { Button, Card, CardBody, Flex, Image, Show, Text } from "@chakra-ui/react";
 
 interface IShowCardSelectableProps {
   show: IShow;
@@ -8,14 +8,15 @@ interface IShowCardSelectableProps {
 }
 
 export default function ShowCardSelectable({show, onClick, isSelected=false}: IShowCardSelectableProps) {
-  const {image_url, title, average_rating} = show;
+  const {image_url, title} = show;
   return (
     <Card>
       <CardBody onClick={() => onClick(show)} background={isSelected ? "green" : "white"} minW="100px">
         <Flex flexDir='column'>
-          <Image src={image_url} objectFit='cover' boxSize='200px'/>
+          <Show above="sm">
+            <Image src={image_url} objectFit='cover' boxSize='200px'/>
+          </Show>
           <Text>{title}</Text>
-          <Text>{average_rating}</Text>
         </Flex>
       </CardBody>
     </Card>

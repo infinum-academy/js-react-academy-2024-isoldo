806313780112eb05d7c8d639ab8b32ba3d379444
Ivan Soldo <ivan@calimero.network>
2024-07-29 18:25:18 +0200

    use RHF controller for rating input

--- tv-show/src/components/features/review/ReviewForm/ReviewForm.tsx
+++ tv-show/src/components/features/review/ReviewForm/ReviewForm.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { Button, Flex, Textarea } from "@chakra-ui/react";
-import { useState } from "react";
 import RatingInput from "../../rating/RatingInput/RatingInput";
 import { INewReview } from "@/typings/Review.type";
 import { IUser } from "@/typings/User.type";
+import { Controller, useForm } from "react-hook-form";
 
 interface IReviewFormProps {
   onSubmit: (newReview: INewReview) => void;
@@ -12,31 +12,39 @@ interface IReviewFormProps {
 }
 
 export default function ReviewForm({onSubmit}: IReviewFormProps) {
-  const [comment, setComment] = useState('');
-  const [rating, setRating] = useState(0);
+  const {register, handleSubmit, control, setValue, formState: {isSubmitting}} = useForm<INewReview>();
+
+  const onRate = (data: INewReview) => {
+    onSubmit(data);
+    setValue("rating", 0);
+    setValue("comment", "");
+  };
 
   return(
     <Flex bg="darkPurple" direction="column" marginBottom={4} gap={3} flexGrow={1} id="review-form">
-      <Textarea
-        borderRadius="26px"
-        bg="white"
-        _placeholder={{color: "lightPurple", opacity: 1}}
-        placeholder='Enter review'
-        color="darkPurple"
-        onBlur={(e) => setComment(e.target.value)}
-      />
-      <Flex justifyContent='space-between' alignItems="baseline" flexGrow={1} className="review-form-rating-row">
-        <RatingInput label='Rating' onChange={(n: number) => setRating(n)} value={rating}/>
-        <Button onClick={() => {
-          const newReview: INewReview = {
-            comment,
-            rating,
-          };
-          setRating(0);
-          setComment('');
-          onSubmit(newReview);
-          }}>Post</Button>
-      </Flex>
+      <form onSubmit={handleSubmit(onRate)}>
+          <Textarea
+            borderRadius="26px"
+            bg="white"
+            _placeholder={{color: "lightPurple", opacity: 1}}
+            placeholder='Enter review'
+            color="darkPurple"
+            {...register("comment")}
+          />
+          <Flex justifyContent='space-between' alignItems="baseline" flexGrow={1} className="review-form-rating-row">
+          <Controller
+              control={control}
+              name="rating"
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <RatingInput
+                  label="rating-label"
+                  onChange={onChange}
+                  value={value}
+                />
+              )} />
+            <Button isDisabled={isSubmitting} type="submit">Post</Button>
+          </Flex>
+      </form>
     </Flex>
   )
 }

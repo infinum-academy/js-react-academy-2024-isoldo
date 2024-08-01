7433c7a8de5b854949d2b0271396fad149fa3810
Ivan Soldo <ivan@calimero.network>
2024-08-01 12:10:56 +0200

    mobile login

--- tv-show/src/components/features/auth/LoginForm/LoginForm.tsx
+++ tv-show/src/components/features/auth/LoginForm/LoginForm.tsx
@@ -2,8 +2,9 @@
 
 import { loginPost, universalFetcher } from "@/fetchers/fetcher";
 import { swrKeys } from "@/fetchers/swrKeys";
-import { Button, Container, Flex, FormControl, FormLabel, Heading, Input, Modal, ModalContent, Text } from "@chakra-ui/react";
+import { Button, Container, Flex, FormControl, FormLabel, Heading, Input, Modal, ModalContent, Show, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import { ReactNode } from "react";
 import { useForm } from "react-hook-form";
 import useSWR from "swr";
 import useSWRMutation from "swr/mutation";
@@ -14,6 +15,23 @@ interface ILoginFormInputs {
 }
 
 export default function LoginForm() {
+  return (
+    <>
+    <Show above="md">
+      <DesktopLoginWrapper>
+        <LoginFormInner />
+      </DesktopLoginWrapper>
+    </Show>
+    <Show below="md">
+      <MobileLoginWrapper>
+        <LoginFormInner />
+      </MobileLoginWrapper>
+    </Show>
+  </>
+  )
+}
+
+function LoginFormInner() {
   const { register, handleSubmit } = useForm<ILoginFormInputs>();
   const { mutate } = useSWR(swrKeys.user(), universalFetcher);
   const { trigger } = useSWRMutation(swrKeys.login(), loginPost, {
@@ -28,28 +46,42 @@ export default function LoginForm() {
   };
 
   return (
+      <Flex direction="column" justifyContent="center" h="100%">
+        <Heading marginBottom={8} as="h2" textAlign="center">TV Show App</Heading>
+        <Flex as="form" direction="column" gap={3} onSubmit={handleSubmit(onLogin)}>
+          <FormControl isRequired>
+            <FormLabel color="white">Email</FormLabel>
+            <Input {...register('email')} required type="email" />
+          </FormControl>
+          <FormControl>
+            <FormLabel color="white">Password</FormLabel>
+            <Input {...register('password')} required type="password" />
+          </FormControl>
+          <Container centerContent>
+            <Button width="fit-content" type="submit">Login</Button>
+          </Container>
+          <Text color="white">Don't have an account? <Link href="/register"><b>Register</b></Link></Text>
+        </Flex>
+      </Flex>
+  )
+}
+
+function DesktopLoginWrapper({children}: {children: ReactNode}) {
+  return (
     <Modal isCentered isOpen={true} onClose={() => null}>
       <ModalContent bg="darkPurple">
         <Container centerContent w="500px" h="500px">
-          <Flex direction="column" justifyContent="center" h="100%">
-            <Heading marginBottom={8} as="h2" color="white">TV Show App</Heading>
-            <Flex as="form" direction="column" gap={3} onSubmit={handleSubmit(onLogin)}>
-              <FormControl isRequired>
-                <FormLabel color="white">Email</FormLabel>
-                <Input {...register('email')} required type="email" />
-              </FormControl>
-              <FormControl>
-                <FormLabel color="white">Password</FormLabel>
-                <Input {...register('password')} required type="password" />
-              </FormControl>
-              <Container centerContent>
-                <Button width="fit-content" type="submit">Login</Button>
-              </Container>
-              <Text color="white">Don't have an account? <Link href="/register"><b>Register</b></Link></Text>
-            </Flex>
-          </Flex>
+          {children}
         </Container>
       </ModalContent>
     </Modal>
+  )
+}
+
+function MobileLoginWrapper({children}: {children: ReactNode}) {
+  return (
+    <Flex bg="purple" h="100vh" justifyContent="center">
+      {children}
+    </Flex>
   )
 }

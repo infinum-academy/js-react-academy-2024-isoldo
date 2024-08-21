acfed6e6ee9e5ca3fed540ae134e5781807c2620
Ivan Soldo <ivan@calimero.network>
2024-08-21 20:24:23 +0200

    change text for step before final stel

--- tv-show/src/components/features/picker/Picker/components/PickerNavButtons.tsx
+++ tv-show/src/components/features/picker/Picker/components/PickerNavButtons.tsx
@@ -10,6 +10,7 @@ export function PickerNavButtons({onClose}: IPickerNavButtonsProps) {
   const ctx = useContext(PickerContext);
 
   const isPrevDisabled = ctx.currentStep === 0;
+  const isNextStepFinal = ctx.isFinalStep(ctx.currentStep + 1);
 
   const onPrevClick = () => {
     ctx.setCurrentStep(ctx.currentStep-1);
@@ -23,10 +24,10 @@ export function PickerNavButtons({onClose}: IPickerNavButtonsProps) {
     <Flex width="100%" justifyContent="space-between">
       {
         ctx.isFinalStep(ctx.currentStep) &&
-        <Button onClick={onClose}>Close</Button>||
+        <Button onClick={onClose}>Close</Button> ||
           <>
           <Button variant="outline" isDisabled={isPrevDisabled} onClick={onPrevClick}>Previous</Button>
-          <Button variant="outline" onClick={onNextClick}>Next</Button>
+          <Button variant="outline" onClick={onNextClick}>{isNextStepFinal ? "Generate watchlist" : "Next"}</Button>
           </>
       }
     </Flex>
